6cb589b987e8d0122296cb0dc53b01bbb02d9ba3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-05-05 13:19:20 +0200

    complete slides on integration tests

---
+++ slides/snippets/15-integration-testing-component.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useReducer } from 'react';
+import { processChartPoint } from './process-chart-point';
+import { getGlobalTemperatureChartData } from './get-global-temperature-chart-data';
+
+function chartReducer(state, action) {
+  switch (action.type) {
+    case 'success':
+      return {
+        ...state,
+        loading: false,
+        chartData: action.chartData,
+      };
+
+    case 'error':
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const GlobalTemperatureChart = ({ fetch }) => {
+  const [{ loading, chartData, error }, dispatch] = useReducer(chartReducer, {
+    loading: true,
+  });
+
+  useEffect(() => {
+    getGlobalTemperatureChartData(fetch, processChartPoint)
+      .then((processedData) => {
+        dispatch({
+          type: 'success',
+          chartData: processedData,
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: 'error',
+          error,
+        });
+      });
+  }, [fetch]);
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  if (error) {
+    return <div>An error occurred while downloading data</div>;
+  }
+
+  return <div>Got data: {chartData.length}</div>;
+};
---
+++ slides/snippets/15-integration-testing-tests.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, cleanup, waitForElement, act } from 'react-testing-library';
+import { GlobalTemperatureChart } from './chart';
+
+afterEach(cleanup);
+
+describe('GlobalTemperatureChart', () => {
+  it('should initially render the loading info', (done) => {
+    const fakeFetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve([]),
+      }),
+    );
+
+    act(async () => {
+      const { getByText } = render(
+        <GlobalTemperatureChart fetch={fakeFetch} />,
+      );
+
+      expect(getByText('Loading')).toBeDefined();
+
+      await waitForElement(() => getByText('Got data: 0'));
+      done();
+    });
+  });
+});
