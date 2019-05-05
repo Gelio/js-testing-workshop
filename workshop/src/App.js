6bb689d5d84e9d1a4641c326c962239a749b8f2b
Grzegorz Rozdzialik <grzegorz.rozdzialik@codilime.com>
2019-05-05 22:58:59 +0200

    add cypress

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div className="App">Dummy content</div>;
+  const [counter, setCounter] = useState(0);
+  const [text, setText] = useState('');
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => setCounter(counter + 1)}
+        className="increment-counter"
+      >
+        +
+      </button>
+      <span className="counter-value">{counter}</span>
+
+      <br />
+
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+        className="example-text-input"
+      />
+      <span className="reflected-text">{text}</span>
+    </div>
+  );
 }
 
 export default App;
